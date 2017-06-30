83bb4fffae0149fcf1ea466278a50dffa8606ef6
Yepifanova Natalia <nun.box@gmail.com>
2017-06-30 12:49:39 +0300

    MatrixPrinter added

--- src/com/homework/Lecture3/MatrixPrinter.java
+++ src/com/homework/Lecture3/MatrixPrinter.java
@@ -1,15 +1,45 @@
 package com.homework.Lecture3;
 
 
-public class MatrixPrinter {
+public abstract class MatrixPrinter {
     public static void main(String[] args) {
         int[][] array = {
-                {1, 2000, 3,    4},
-                {3, 4,    5250, 60000000},
-                {7, 5,    4,    1}
+                {1, 200, 3, -4},
+                {3, 4, 5250, 60000},
+                {7, 5, 4, 1}
         };
 
+        int maxValue = 0;
+        boolean hasNegative = false;
 
-    // Даже не представляю, как это сделать
+        for (int[] row : array) {
+            for (int element : row) {
+                int temp = element;
+                if (element < 0) {
+                    hasNegative = true;
+                    temp = -temp;
+                }
+                if (temp > maxValue) {
+                    maxValue = temp;
+                }
+            }
+        }
+        int len = Integer.toString(maxValue).length() + 1;
+
+        if (hasNegative) {
+            ++len;
+        }
+
+        final String formatString = "%" + (-len) + "d";
+
+        for (int i = 0; i < array.length; ++i) {
+            for (int j = 0; j < array[0].length; ++j) {
+                System.out.printf(formatString, array[i][j]);
+            }
+                System.out.println();
+        }
     }
 }
+
+
+
