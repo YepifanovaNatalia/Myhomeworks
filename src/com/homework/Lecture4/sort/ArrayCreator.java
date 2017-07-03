0db129924d9a3b41dac237acf06162c3f2088844
Yepifanova Natalia <nun.box@gmail.com>
2017-07-03 22:21:47 +0300

    HugeArraySorter was added

--- src/com/homework/Lecture4/sort/ArrayCreator.java
+++ src/com/homework/Lecture4/sort/ArrayCreator.java
@@ -3,12 +3,25 @@ package com.homework.Lecture4.sort;
 import java.util.Random;
 
 public class ArrayCreator {
-    public static int[] createHugeArray(int n) {
+   public static int[] createHugeArray(int n) {
+        Random random = new Random();
+        int[] result = new int[n];
+        for(int index = 0; index < result.length; index++) {
+            result[index] = random.nextInt((1_000_00) + 1_000_00);
+        }
+        return result;
+    }
+
+
+
+
+
+    /*public static int[] createHugeArray(int n) {
         Random random = new Random();
         int[] result = new int[random.nextInt(1_000_00) + 1_000_00];
         for(int index = 0; index < result.length; index++) {
             result[index] = random.nextInt(n);
         }
         return result;
-    }
+    }*/
 }
--- src/com/homework/Lecture4/sort/HugeArraySorter.java
+++ src/com/homework/Lecture4/sort/HugeArraySorter.java
@@ -1,10 +1,15 @@
 package com.homework.Lecture4.sort;
 
-public class HugeArraySorter {
-    public static void main(String[] args) {
-        int[] hugeArray = ArrayCreator.createHugeArray(10);
+import java.util.Arrays;
 
-        /*Your implementation here*/
+
+public class HugeArraySorter {
+    static int[] hugeArray = ArrayCreator.createHugeArray(10);
+
+    public static void main(String[] args) {
+
+        sort(hugeArray);
+
 
         for (int index = 0; index < hugeArray.length - 1; index++) {
             if (hugeArray[index] > hugeArray[index + 1]) {
@@ -12,7 +17,46 @@ public class HugeArraySorter {
                 return;
             }
         }
-
+        System.out.println(Arrays.toString(hugeArray));
         System.out.println("Sort successful!!");
     }
+
+
+    public static void sort(int[] array) {
+        int startIndex = 0;
+        int endIndex = array.length - 1;
+        doSort(hugeArray, startIndex, endIndex);
+    }
+
+    private static void doSort(int[] array, int start, int end) {
+        if (start >= end)
+            return;
+        int i = start, j = end;
+        int cur = i - (i - j) / 2;
+        while (i < j) {
+            while (i < cur && (array[i] <= array[cur])) {
+                i++;
+            }
+            while (j > cur && (array[cur] <= array[j])) {
+                j--;
+            }
+            if (i < j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                if (i == cur)
+                    cur = j;
+                else if (j == cur)
+                    cur = i;
+            }
+        }
+        doSort(hugeArray, start, cur);
+        doSort(hugeArray,cur + 1, end);
+    }
 }
+
+
+
+
+
+
